cae262a2c752c71b20e3abb9e7a084d50d0a88c6
Vitor Gabriel <143555648+vitorpdf@users.noreply.github.com>
2026-06-15 23:28:55 -0300

    Update

--- amd/build/report.min.js
+++ amd/build/report.min.js
@@ -1,17 +1,4 @@
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
 
-/**
- * AMD module – Gemini Data Reports.
- *
- * @module    report_gemini_data/report
- * @copyright 2025
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
 define(['jquery'], function($) {
     'use strict';
 
