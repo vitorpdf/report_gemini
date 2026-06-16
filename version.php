385722f1bfc71ae2d75c787702c9d030431caf7e
Vitor Gabriel <143555648+vitorpdf@users.noreply.github.com>
2026-06-15 22:30:20 -0300

    Update version.php

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 
-$plugin->component = 'report_gemini_data';
-$plugin->version   = 2025052900;
+defined('MOODLE_INTERNAL') || die;
+
+$plugin->version   = 2026052300;
 $plugin->requires  = 2022041900; // Moodle 4.0+
-$plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.0.1';
+$plugin->component = 'report_gemini_data';
